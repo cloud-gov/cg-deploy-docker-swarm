23f89043f5ec9866fcc95fb9efb9dde502cf4ba2
Diego Lapiduz <diego.lapiduz@gsa.gov>
2016-05-31 22:22:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 18F cloud.gov Docker Swarm Broker deploymnet
+
+This repo contains the pipeline and [BOSH](https://bosh.io) manifests for deploying cloud.gov [Docker Swarm Broker](https://github.com/cloudfoundry-community/docker-boshrelease) implementation.
---
+++ generate.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e -x
+
+SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+
+SECRETS=$SCRIPTPATH/secrets.yml
+MANIFEST=$SCRIPTPATH/manifest.yml
+if [ ! -z "$1" ]; then
+  SECRETS=$1
+fi
+if [ ! -z "$2" ]; then
+  MANIFEST=$2
+fi
+
+spiff merge \
+  $SCRIPTPATH/deployment.yml \
+  $SCRIPTPATH/services.yml \
+  $SECRETS \
+  > $MANIFEST
